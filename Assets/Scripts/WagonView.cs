94ed79305aff299f9c76be623bd9b7fdc01f606a
Eighty5Unity <Eighty5.Unity@gmail.com>
2022-06-15 10:23:36 +0300

    add script tag for characters and add wagon

---
+++ Assets/Scripts/WagonView.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class WagonView : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.TryGetComponent<BeardedView>(out BeardedView beader))
+        {
+            _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.TryGetComponent<BeardedView>(out BeardedView beader))
+        {
+            _rigidbody.bodyType = RigidbodyType2D.Static;
+        }
+    }
+}
